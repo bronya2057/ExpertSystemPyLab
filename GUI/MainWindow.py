136594749437cc4af51b1442fa38e9e1ade34219
Oleksandr Brodskyi <abrodskyi@luxoft.com>
2018-09-21 16:02:20 +0300

    Add example TreeViewModel

--- GUI/MainWindow.py
+++ GUI/MainWindow.py
@@ -1,8 +1,13 @@
 from PyQt5 import QtGui, QtWidgets, uic
-from PyQt5.QtWidgets import QDialog, QListView
+from PyQt5.QtCore import QDir, QIODevice, QFile
+from PyQt5.QtWidgets import QDialog, QListView, QFileSystemModel, QTreeView
+from PyQt5 import QtCore
 
 
 # slot example
+from GUI.TreeModel import ExpertTreeModel
+
+
 def on_esdouble_spin_box_changed(value):
     print(value)
     print("Activate Double spinbox slot")
@@ -37,7 +42,7 @@ class MyWindow(QtWidgets.QMainWindow):
 class MyExtraDialog(QtWidgets.QDialog):
     def __init__(self, parent=None):
         super(MyExtraDialog, self).__init__(parent)
-        uic.loadUi('Dialog.ui', self)
+        uic.loadUi('TreeView.ui', self)
 
     # static method to create the dialog and return (date, time, accepted)
     @staticmethod
@@ -45,6 +50,23 @@ class MyExtraDialog(QtWidgets.QDialog):
         dialog = MyExtraDialog(parent)
         result = dialog.exec_()
         return result == QDialog.Accepted
+
+    def load_example_file_system(self):
+        treeView = self.findChild(QTreeView, "treeView")
+        model = QFileSystemModel()
+
+        model.setRootPath(QDir.currentPath())
+        treeView.setModel(model)
+
+    def load_example_custom_model(self):
+        f = QFile(QDir.currentPath() + "/default.txt")
+        f.open(QIODevice.ReadOnly)
+        model = ExpertTreeModel(f.readAll())
+        f.close()
+
+        treeView = self.findChild(QTreeView, "treeView")
+        treeView.setModel(model)
+        self.treeView.setColumnWidth(0, 300)
 
 
 def init_gui():
@@ -55,5 +77,16 @@ def init_gui():
     sys.exit(app.exec_())
 
 
+def init_treeView():
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyExtraDialog()
+    #window.load_example_file_system()
+    window.load_example_custom_model()
+    window.show()
+    sys.exit(app.exec_())
+
+
 if __name__ == '__main__':
-    init_gui()
+    # init_gui() # Main window then TreeView
+    init_treeView()  # only treeView
